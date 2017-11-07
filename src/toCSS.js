cf6ffc6f54c0ce6e521526f22b18af3a3bfb5a03
Ayc0 <benjamin.koltes@gmail.com>
2017-11-07 02:44:07 +0100

    toCSS function

---
+++ src/toCSS.js
@@ -0,0 +1,26 @@
+import BreakpointsProvider from './BreakpointsProvider';
+
+const toCSS = (points) => {
+  if (typeof points !== 'object') {
+    throw new Error('Invalid breakpoints, should be an object');
+  }
+  const css = {};
+  Object.keys(points).forEach((point) => {
+    if (!Object.keys(BreakpointsProvider.breakpoints).includes(point)) {
+      throw new Error(`${point} is not a valid breakpoint\nValid breakpoints are: ${Object.keys(BreakpointsProvider.breakpoints)}`);
+    }
+    if (typeof points[point] !== 'object') {
+      throw new Error('Invalid CSS-in-JS, should be an object');
+    }
+    if (BreakpointsProvider.breakpoints[point][0] === 0) {
+      Object.keys(points[point]).forEach((cssParam) => {
+        css[cssParam] = points[point][cssParam];
+      });
+    } else {
+      css[`@media (min-width: ${BreakpointsProvider.breakpoints[point][0]}px)`] = points[point];
+    }
+  });
+  return css;
+};
+
+export default toCSS;
