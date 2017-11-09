93a8368ccefd8cf75c8c7413559928b6fb43703b
Ayc0 <benjamin.koltes@gmail.com>
2017-11-09 03:02:48 +0100

    Sanitize props of BreakpointsProvider

--- src/BreakpointsProvider.js
+++ src/BreakpointsProvider.js
@@ -1,12 +1,56 @@
 import { Component } from 'react';
 
+const listOfSupportedUnits = [
+  'em',
+  'ex',
+  '%',
+  'px',
+  'cm',
+  'mm',
+  'in',
+  'pt',
+  'pc',
+  'ch',
+  'rem',
+  'vh',
+  'vw',
+  'vmin',
+  'vmax',
+];
+
+const sanitize = (obj) => {
+  const outObj = {};
+  Object.keys(obj).forEach((breakpointName) => {
+    const breakpoint = obj[breakpointName];
+    if (!Array.isArray(breakpoint) || breakpoint.length <= 1) {
+      return;
+    }
+    const [supposedMin, supposedMax, supposedUnit, ...rest] = breakpoint;
+    if (rest.length > 0) {
+      const error = new Error(`The following fields "${rest}" have been ignored`);
+      console.error(error);
+    }
+    if (typeof supposedMin !== 'number' || typeof supposedMax !== 'number') {
+      return;
+    }
+    const min = Math.min(supposedMin, supposedMax);
+    const max = Math.max(supposedMin, supposedMax);
+    const unit = supposedUnit && listOfSupportedUnits.includes(supposedUnit) ? supposedUnit : 'px';
+    outObj[breakpointName] = [min, max, unit];
+  });
+  return outObj;
+};
+
 class BreakpointsProvider extends Component {
   static breakpoints = {};
 
   constructor(props) {
     super(props);
 
-    BreakpointsProvider.breakpoints = Object.assign(props.breakpoints, props.additionalBreakpoints);
+    const breakpoints = sanitize(props.breakpoints);
+    const additionalBreakpoints = sanitize(props.additionalBreakpoints);
+
+    BreakpointsProvider.breakpoints = Object.assign(breakpoints, additionalBreakpoints);
   }
   render() {
     return this.props.children;
@@ -15,11 +59,11 @@ class BreakpointsProvider extends Component {
 
 BreakpointsProvider.defaultProps = {
   breakpoints: {
-    xs: [0, 576], // Extra small devices (portrait phones)
-    sm: [576, 768], // Small devices (landscape phones)
-    md: [768, 992], // Medium devices (tablets)
-    lg: [992, 1200], // Large devices (desktops)
-    xl: [1200, Infinity], // Extra large devices (large desktops)
+    xs: [0, 576, 'px'], // Extra small devices (portrait phones)
+    sm: [576, 768, 'px'], // Small devices (landscape phones)
+    md: [768, 992, 'px'], // Medium devices (tablets)
+    lg: [992, 1200, 'px'], // Large devices (desktops)
+    xl: [1200, Infinity, 'px'], // Extra large devices (large desktops)
   },
   additionalBreakpoints: {},
   children: () => {},
