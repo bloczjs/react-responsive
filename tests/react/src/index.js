95c03da09899644d5f27105eead13274b266d4cc
Benjamin Koltes <benjamin.koltes@datadoghq.com>
2019-06-07 14:47:26 -0400

    change example

--- tests/react/src/index.js
+++ tests/react/src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import List from "./List";
-import Custom from "./Custom";
-import MediaQuery from "./MediaQuery";
-import Match from "./Match";
+import List from './List';
+import Custom from './Custom';
+import MediaQuery from './MediaQuery';
+import Match from './Match';
+import Hook from './Hook';
 
 const App = (
   <React.Fragment>
@@ -12,7 +13,8 @@ const App = (
     <Custom />
     <MediaQuery />
     <Match />
+    <Hook />
   </React.Fragment>
 );
 
-ReactDOM.render(App, document.getElementById("root"));
+ReactDOM.render(App, document.getElementById('root'));
