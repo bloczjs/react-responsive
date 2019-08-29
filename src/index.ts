a0997095c6e158a270b67e72f75e34a187a0924e
Benjamin Koltes <benjamin.koltes@datadoghq.com>
2019-08-29 18:43:55 -0400

    fix build + export context

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { BreakpointsProvider } from "./BreakpointsContext";
+export { BreakpointsProvider, BreakpointsContext } from "./BreakpointsContext";
 export { toJSON } from "./toJSON";
 export { toCSS } from "./toCSS";
 export { toMediaQuery } from "./toMediaQuery";
