555a5c65e4159984ee8666ee3b2d6536beb023d3
Benjamin Koltes <benjamin.koltes@datadoghq.com>
2019-09-16 18:18:51 -0400

    refactor sanitize

--- src/sanitize.ts
+++ src/sanitize.ts
@@ -39,32 +39,40 @@ export interface Breakpoints {
   [key: string]: Breakpoint;
 }
 
-export const sanitize = (obj: Breakpoints) => {
-  const outObj: Breakpoints = {};
-  Object.keys(obj).forEach(breakpointName => {
-    const breakpoint = obj[breakpointName];
-    if (!Array.isArray(breakpoint) || breakpoint.length <= 1) {
-      return;
-    }
-    const [supposedMin, supposedMax, supposedUnit, ...rest] = breakpoint;
-    if (rest.length > 0) {
-      const error = new Error(
-        `The following fields "${rest}" have been ignored`
-      );
-      console.error(error);
-    }
-    if (typeof supposedMin !== "number" || typeof supposedMax !== "number") {
-      return;
-    }
-    const min = Math.min(supposedMin, supposedMax);
-    const max = Math.max(supposedMin, supposedMax);
-    const unit: Units =
-      supposedUnit && listOfSupportedUnits.includes(supposedUnit)
-        ? supposedUnit
-        : "px";
-    outObj[breakpointName] = [min, max, unit];
-    outObj[`${breakpointName}Up`] = [min, Infinity, unit];
-    outObj[`${breakpointName}Down`] = [0, max, unit];
-  });
-  return outObj;
+export const sanitize = (inBreakpoints: Breakpoints) => {
+  return Object.keys(inBreakpoints).reduce<Breakpoints>(
+    (breakpoints, breakpointName) => {
+      const breakpoint = inBreakpoints[breakpointName];
+
+      if (!Array.isArray(breakpoint) || breakpoint.length <= 1) {
+        return breakpoints;
+      }
+
+      const [supposedMin, supposedMax, supposedUnit, ...rest] = breakpoint;
+      if (rest.length > 0) {
+        const error = new Error(
+          `The following fields "${rest}" have been ignored`
+        );
+        console.error(error);
+      }
+
+      if (typeof supposedMin !== "number" || typeof supposedMax !== "number") {
+        return breakpoints;
+      }
+
+      const min = Math.min(supposedMin, supposedMax);
+      const max = Math.max(supposedMin, supposedMax);
+      const unit: Units =
+        supposedUnit && listOfSupportedUnits.includes(supposedUnit)
+          ? supposedUnit
+          : "px";
+
+      breakpoints[breakpointName] = [min, max, unit];
+      breakpoints[`${breakpointName}Up`] = [min, Infinity, unit];
+      breakpoints[`${breakpointName}Down`] = [0, max, unit];
+
+      return breakpoints;
+    },
+    {}
+  );
 };
