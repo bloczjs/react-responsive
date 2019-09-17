9bd744fce5ba6724ca1250206837639a43a3ef0b
Benjamin Koltes <benjamin.koltes@datadoghq.com>
2019-09-17 16:43:14 -0400

    add every breakpoints

--- tests/src/Hook.tsx
+++ tests/src/Hook.tsx
@@ -19,12 +19,21 @@ const Hook = () => {
   const isMd = useOnly("md");
   const isLg = useOnly("lg");
   const isXl = useOnly("xl");
+  const isXsUp = useOnly("xsUp");
+  const isSmUp = useOnly("smUp");
+  const isMdUp = useOnly("mdUp");
+  const isLgUp = useOnly("lgUp");
+  const isXlUp = useOnly("xlUp");
+  const isXsDown = useOnly("xsDown");
+  const isSmDown = useOnly("smDown");
+  const isMdDown = useOnly("mdDown");
+  const isLgDown = useOnly("lgDown");
+  const isXlDown = useOnly("xlDown");
   const isMedia = useOnly(
     undefined,
     "(min-width:768px) and (max-width:992px),(max-width:576px)"
   );
 
-  const isSmUp = useOnly("smUp");
   return (
     <>
       <h2>useOnly()</h2>
@@ -33,24 +42,60 @@ const Hook = () => {
         <code>{toString(isXs)}</code>
       </p>
       <p>
+        <b>isXsDown: </b>
+        <code>{toString(isXsDown)}</code>
+      </p>
+      <p>
+        <b>isXsUp: </b>
+        <code>{toString(isXsUp)}</code>
+      </p>
+      <p>
         <b>isSm: </b>
         <code>{toString(isSm)}</code>
+      </p>
+      <p>
+        <b>isSmDown: </b>
+        <code>{toString(isSmDown)}</code>
+      </p>
+      <p>
+        <b>isSmUp: </b>
+        <code>{toString(isSmUp)}</code>
       </p>
       <p>
         <b>isMd: </b>
         <code>{toString(isMd)}</code>
       </p>
       <p>
+        <b>isMdDown: </b>
+        <code>{toString(isMdDown)}</code>
+      </p>
+      <p>
+        <b>isMdUp: </b>
+        <code>{toString(isMdUp)}</code>
+      </p>
+      <p>
         <b>isLg: </b>
         <code>{toString(isLg)}</code>
+      </p>
+      <p>
+        <b>isLgDown: </b>
+        <code>{toString(isLgDown)}</code>
+      </p>
+      <p>
+        <b>isLgUp: </b>
+        <code>{toString(isLgUp)}</code>
       </p>
       <p>
         <b>isXl: </b>
         <code>{toString(isXl)}</code>
       </p>
       <p>
-        <b>isSmUp: </b>
-        <code>{toString(isSmUp)}</code>
+        <b>isXlDown: </b>
+        <code>{toString(isXlDown)}</code>
+      </p>
+      <p>
+        <b>isXlUp: </b>
+        <code>{toString(isXlUp)}</code>
       </p>
       <p>
         <b>(min-width:768px) and (max-width:992px),(max-width:576px): </b>
