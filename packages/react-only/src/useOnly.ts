07452e445c7dae9650ac3c71a662b58295fcb284
Ayc0 <ayc0.benj@gmail.com>
2020-03-06 23:28:59 -0500

    [useOnly] remove media query param + useQuery under the hood + refactor Only

--- packages/react-only/src/useOnly.ts
+++ packages/react-only/src/useOnly.ts
@@ -3,30 +3,14 @@ import * as React from "react";
 import { BreakpointsContext } from "./BreakpointsContext";
 
 import { mediaQueryBuilder } from "./mediaQueryBuilder";
-import matchMedia from "./matchMediaPonyfill";
 
-export const useOnly = (on?: string, media?: string, strict?: boolean) => {
-  const [isShown, setIsShown] = React.useState<boolean | undefined>(undefined);
+import { useQuery } from "./useQuery";
 
+export const useOnly = (on?: string, strict?: boolean) => {
   const breakpoints = React.useContext(BreakpointsContext);
   const toMediaQuery = React.useMemo(() => mediaQueryBuilder(breakpoints), [breakpoints]);
 
-  const mediaQuery = React.useMemo(() => toMediaQuery(on, media, strict), [toMediaQuery, on, media, strict]);
+  const mediaQuery = React.useMemo(() => toMediaQuery(on, strict), [toMediaQuery, on, strict]);
 
-  React.useLayoutEffect(() => {
-    if (!mediaQuery) {
-      return;
-    }
-    const currentMatchMedia = matchMedia(mediaQuery);
-    setIsShown(currentMatchMedia.matches);
-    const listener = (event: MediaQueryListEvent) => {
-      setIsShown(event.matches);
-    };
-    currentMatchMedia.addListener(listener);
-    return () => {
-      currentMatchMedia.removeListener(listener);
-    };
-  }, [mediaQuery]);
-
-  return isShown;
+  return useQuery(mediaQuery);
 };
