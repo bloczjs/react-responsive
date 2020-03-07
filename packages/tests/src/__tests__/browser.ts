d668279b085edab27877d0e5d8fcd1ba70636b39
Ayc0 <ayc0.benj@gmail.com>
2020-03-07 00:38:14 -0500

    [test] export sizes

--- packages/tests/src/__tests__/browser.ts
+++ packages/tests/src/__tests__/browser.ts
@@ -1,5 +1,7 @@
 import "../App";
 
+import { sizes } from "./sizes";
+
 const sleep = (ms: number) =>
   new Promise(res => setTimeout(res, ms));
 
@@ -16,38 +18,8 @@ const getText = async () => {
 it("browser test", async () => {
   await page.goto("http://localhost:3000");
 
-  // on extra small device
-  await page.setViewport({
-    width: 500,
-    height: 300,
-  });
-  expect(await getText()).toMatchSnapshot();
-
-  // on small device
-  await page.setViewport({
-    width: 750,
-    height: 550,
-  });
-  expect(await getText()).toMatchSnapshot();
-
-  // on medium device
-  await page.setViewport({
-    width: 900,
-    height: 700,
-  });
-  expect(await getText()).toMatchSnapshot();
-
-  // on large device
-  await page.setViewport({
-    width: 1000,
-    height: 800,
-  });
-  expect(await getText()).toMatchSnapshot();
-
-  // on extra large device
-  await page.setViewport({
-    width: 1300,
-    height: 1100,
-  });
-  expect(await getText()).toMatchSnapshot();
+  for (const size of sizes) {
+    await page.setViewport(size);
+    expect(await getText()).toMatchSnapshot();
+  }
 });
---
+++ packages/tests/src/__tests__/sizes.ts
@@ -0,0 +1,31 @@
+export const sizes = [
+  // on extra small device
+  {
+    width: 500,
+    height: 300,
+  },
+
+  // on small device
+  {
+    width: 750,
+    height: 550,
+  },
+
+  // on medium device
+  {
+    width: 900,
+    height: 700,
+  },
+
+  // on large device
+  {
+    width: 1000,
+    height: 800,
+  },
+
+  // on extra large device
+  {
+    width: 1300,
+    height: 1100,
+  },
+];
