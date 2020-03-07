13d3b29ae8973b6c9629c2ba8ffae7b4882e7352
Ayc0 <ayc0.benj@gmail.com>
2020-03-06 23:42:01 -0500

    [ssr] change rendering

--- packages/tests/src/__tests__/ssr.ts
+++ packages/tests/src/__tests__/ssr.ts
@@ -3,5 +3,10 @@ import { renderToString } from "react-dom/server";
 import App from "../App";
 
 it("Should render in SSR", () => {
-  expect(renderToString(App)).toMatchSnapshot();
+  expect(
+    renderToString(App)
+      .replace(/<\/[^>]*>/g, match => `${match}\n`)
+      .replace(/<[^>]*\/>/g, match => `\n${match}\n`)
+      .replace(/<[^/>]*>/g, match => `\n${match}`),
+  ).toMatchSnapshot();
 });
