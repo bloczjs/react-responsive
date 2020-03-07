2d37ad592c3105210ea3eb198b4992a4d19870d0
Ayc0 <ayc0.benj@gmail.com>
2020-03-06 23:28:07 -0500

    [useQuery] add new hook

--- packages/react-only/src/index.ts
+++ packages/react-only/src/index.ts
@@ -2,4 +2,5 @@ export { BreakpointsProvider, BreakpointsContext } from "./BreakpointsContext";
 export { Match, MatchChildProps } from "./Match";
 export { Only } from "./Only";
 export { useOnly } from "./useOnly";
+export { useQuery } from "./useQuery";
 export { Units } from "./sanitize";
