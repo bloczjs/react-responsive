e6786771498a5c011ed33fb28712c11eb268c000
Ayc0 <ayc0.benj@gmail.com>
2022-06-07 01:13:03 +0200

    prettier

--- packages/react-responsive/src/mediaQueryBuilder.ts
+++ packages/react-responsive/src/mediaQueryBuilder.ts
@@ -1,23 +1,25 @@
 import { Breakpoints } from "./sanitize";
 import { fromBreakpointToMedia } from "./fromBreakpointToMedia";
 
-export const mediaQueryBuilder = (breakpoints: Breakpoints) => (on = ""): string => {
-  if (!on) {
-    return "";
-  }
-  const rawBreakpointNames = on.split(" ");
-  const filteredBreakpoints = rawBreakpointNames.map((breakpointName) => breakpoints[breakpointName]).filter(Boolean);
-  const mediaQuery = filteredBreakpoints
-    .map((breakpoint) => fromBreakpointToMedia(breakpoint))
-    .filter(Boolean)
-    .join(",");
-  if (!mediaQuery) {
-    const isUniqBreakpoint = rawBreakpointNames.length === 1;
-    console.error(
-      `"${rawBreakpointNames.join('", "')}" ${isUniqBreakpoint ? "is" : "are"}n't ${
-        isUniqBreakpoint ? "a " : ""
-      }valid breakpoint${isUniqBreakpoint ? "" : "s"}`,
-    );
-  }
-  return mediaQuery;
-};
+export const mediaQueryBuilder =
+  (breakpoints: Breakpoints) =>
+  (on = ""): string => {
+    if (!on) {
+      return "";
+    }
+    const rawBreakpointNames = on.split(" ");
+    const filteredBreakpoints = rawBreakpointNames.map((breakpointName) => breakpoints[breakpointName]).filter(Boolean);
+    const mediaQuery = filteredBreakpoints
+      .map((breakpoint) => fromBreakpointToMedia(breakpoint))
+      .filter(Boolean)
+      .join(",");
+    if (!mediaQuery) {
+      const isUniqBreakpoint = rawBreakpointNames.length === 1;
+      console.error(
+        `"${rawBreakpointNames.join('", "')}" ${isUniqBreakpoint ? "is" : "are"}n't ${
+          isUniqBreakpoint ? "a " : ""
+        }valid breakpoint${isUniqBreakpoint ? "" : "s"}`,
+      );
+    }
+    return mediaQuery;
+  };
