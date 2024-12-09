277127ad6d77eb1fcbd03d8437609d477c375266
Ayc0 <ayc0.benj@gmail.com>
2024-12-09 19:26:23 +0100

    use with instead of assert

--- packages/tests/src/__tests__/esm.mjs
+++ packages/tests/src/__tests__/esm.mjs
@@ -1,4 +1,4 @@
 import * as ReactResponsive from "@blocz/react-responsive";
-import packageJSON from "@blocz/react-responsive/package.json" assert { type: "json" };
+import packageJSON from "@blocz/react-responsive/package.json" with { type: "json" };
 
 console.log("Didn’t crash");
