74d54fc24c6c0d74d27aba776761dbf981c6e9e6
Ayc0 <ayc0.benj@gmail.com>
2026-05-31 10:35:40 +0200

    [oxlint] remove plugin typescript

--- oxlintrc.config.ts
+++ oxlintrc.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "oxlint";
 
 export default defineConfig({
-  plugins: ["typescript"],
   jsPlugins: ["eslint-plugin-prettier"],
   categories: {
     correctness: "error",
